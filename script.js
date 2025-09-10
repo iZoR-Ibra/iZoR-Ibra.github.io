cafdb3b7feb31f800af7ac6c7866a3607a7bc029
Ibragim Zordunov <izor2005usa@gmail.com>
2025-09-10 21:18:55 +0500

    Add link to the project's repository

--- script.js
+++ script.js
@@ -4,15 +4,27 @@ document.querySelectorAll(".project-link").forEach(link => {
     const parent = this.parentElement;
     const imgDiv = parent.querySelector(".project-image");
 
+    document.querySelectorAll(".project-image").forEach(div => {
+      if (div !== imgDiv) {
+        div.innerHTML = "";
+        div.style.display = "none";
+      }
+    });
+
     if (imgDiv.innerHTML) {
       imgDiv.innerHTML = "";
       imgDiv.style.display = "none";
     } else {
       const url = this.dataset.url || null;
+      const github = this.dataset.github || null;
+
       let content = `<img src="${this.dataset.img}" alt="Project Preview">`;
 
       if (url) {
         content += `<br><a href="${url}" target="_blank" class="view-link">View Project</a>`;
+      }
+      if (github) {
+        content += `<br><a href="${github}" target="_blank" class="view-link">View Code</a>`;
       }
 
       imgDiv.innerHTML = content;
